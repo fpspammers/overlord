5ccbd720f21a9a2586380ec1f18cde31e05dbc06
Atharva <atharva.ek@gmail.com>
2019-07-13 10:32:30 +0530

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const { prefix, token} = require("./config.json");
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+client.on('message', message => {
+    //console.log(message.content)
+
+    if(message.content.startsWith('${prefix}kick')) {
+        //message.channel.send("kick")
+
+        let member = message.mentions.members.first();
+        member.kick().then((member) => {
+            message.channel.send("kick" + member.displayName + "has been kicked!")
+
+        })
+    }
+})
+
+
+client.login(token);
+
